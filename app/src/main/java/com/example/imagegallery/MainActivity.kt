b0a0d4d5dfbe89763621401d7086d07bb11fe441
slain.sh <zachquita@gmail.com>
2025-09-30 10:17:15 +0800

    Username, password, confirm pass, and checkbox

--- app/src/main/java/com/example/imagegallery/MainActivity.kt
+++ app/src/main/java/com/example/imagegallery/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.imagegallery
 
 import android.os.Bundle
+import android.text.InputType
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,25 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val passwordEditText = findViewById<EditText>(R.id.editTextPassword)
+        val confirmPasswordEditText = findViewById<EditText>(R.id.editTextConfirmPassword)
+        val showPasswordCheckbox = findViewById<CheckBox>(R.id.checkboxShowPassword)
+        val passwordEditTexts: Array<EditText> = arrayOf(passwordEditText, confirmPasswordEditText)
+
+        // Toggle hide/show password feature
+        showPasswordCheckbox.setOnClickListener {
+            for (editText in passwordEditTexts) {
+                if (editText.inputType == InputType.TYPE_CLASS_TEXT
+                    or InputType.TYPE_TEXT_VARIATION_PASSWORD)
+                {
+                    editText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                } else {
+                    editText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                }
+                editText.setSelection(editText.text?.length ?: 0)
+            }
         }
     }
 }
