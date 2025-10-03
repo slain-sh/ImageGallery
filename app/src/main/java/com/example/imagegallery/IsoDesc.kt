aecb9d44bc763d2dd1fc1d6ec8c538e76b387faf
slain.sh <zachquita@gmail.com>
2025-10-04 07:49:27 +0800

    Finish ISO activity

--- app/src/main/java/com/example/imagegallery/IsoDesc.kt
+++ app/src/main/java/com/example/imagegallery/IsoDesc.kt
@@ -1,10 +1,13 @@
 package com.example.imagegallery
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
 
 class IsoDesc : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,28 @@ class IsoDesc : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Fixed Calendar no Actions/Events
+        val calendarView = findViewById<CalendarView>(R.id.calendarView)
+
+        val myDate = Calendar.getInstance()
+        myDate.set(2023, Calendar.OCTOBER, 31) // Year, Month (0-based), Day
+        val dateInMillis = myDate.timeInMillis
+
+        calendarView.minDate = dateInMillis
+        calendarView.maxDate = dateInMillis
+
+        calendarView.date = dateInMillis
+
+        calendarView.isEnabled = false
+        calendarView.setOnDateChangeListener { _, _, _, _ ->
+        }
+
+        // Back Button
+        val backButton = findViewById<Button>(R.id.backButton)
+        backButton.setOnClickListener {
+            finish()
+        }
+
     }
 }
