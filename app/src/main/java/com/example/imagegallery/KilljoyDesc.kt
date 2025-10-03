54770ff42d0e64b043af49a8fd4cc9d0dae4521e
treeform-iii <anoob7743@gmail.com>
2025-10-03 22:27:10 +0800

    Pending BUG Fix: Dark Mode Calendar Numbers

--- app/src/main/java/com/example/imagegallery/KilljoyDesc.kt
+++ app/src/main/java/com/example/imagegallery/KilljoyDesc.kt
@@ -1,21 +1,39 @@
 package com.example.imagegallery
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.CalendarView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.*
 
 class KilljoyDesc : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_killjoy_desc)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Fixed Calendar no Actions/Events
+        val calendarView = findViewById<CalendarView>(R.id.calendarView)
+
+        val myDate = Calendar.getInstance()
+        myDate.set(2020, Calendar.AUGUST, 4) // Year, Month (0-based), Day
+        val dateInMillis = myDate.timeInMillis
+
+        calendarView.minDate = dateInMillis
+        calendarView.maxDate = dateInMillis
+
+        calendarView.date = dateInMillis
+
+        calendarView.isEnabled = false
+        calendarView.setOnDateChangeListener { _, _, _, _ ->
         }
     }
 }
