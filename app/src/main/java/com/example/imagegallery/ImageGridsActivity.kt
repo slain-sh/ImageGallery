7a5edcbb6e66fe7c743267aed5a973927e8e4ff2
treeform-iii <anoob7743@gmail.com>
2025-10-03 16:11:37 +0800

    Radio Button Group Added

--- app/src/main/java/com/example/imagegallery/ImageGridsActivity.kt
+++ app/src/main/java/com/example/imagegallery/ImageGridsActivity.kt
@@ -5,16 +5,70 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+import android.widget.RadioGroup
+import android.widget.GridLayout
 
 class ImageGridsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_image_grids)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val agentRoleRadioGroup = findViewById<RadioGroup>(R.id.AgentRolesRadioGroup)
+        val gridLayout = findViewById<GridLayout>(R.id.imageGridLayout)
+
+        val imageViewJett = findViewById<ImageView>(R.id.imageViewJett)
+        val imageViewTejo = findViewById<ImageView>(R.id.imageViewTejo)
+        val imageViewDeadlock = findViewById<ImageView>(R.id.imageViewDeadlock)
+        val imageViewIso = findViewById<ImageView>(R.id.imageViewIso)
+        val imageViewSova = findViewById<ImageView>(R.id.imageViewSova)
+        val imageViewKilljoy = findViewById<ImageView>(R.id.imageViewKilljoy)
+
+        gridLayout.removeAllViews()
+        gridLayout.addView(imageViewJett)
+        gridLayout.addView(imageViewTejo)
+        gridLayout.addView(imageViewDeadlock)
+        gridLayout.addView(imageViewIso)
+        gridLayout.addView(imageViewSova)
+        gridLayout.addView(imageViewKilljoy)
+
+        agentRoleRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.radioAll -> {
+                    gridLayout.removeAllViews()
+                    gridLayout.addView(imageViewJett)
+                    gridLayout.addView(imageViewTejo)
+                    gridLayout.addView(imageViewDeadlock)
+                    gridLayout.addView(imageViewIso)
+                    gridLayout.addView(imageViewSova)
+                    gridLayout.addView(imageViewKilljoy)
+                }
+
+                R.id.radioDuelist -> {
+                    gridLayout.removeAllViews()
+                    gridLayout.addView(imageViewJett)
+                    gridLayout.addView(imageViewIso)
+                }
+
+                R.id.radioSentinel -> {
+                    gridLayout.removeAllViews()
+                    gridLayout.addView(imageViewDeadlock)
+                    gridLayout.addView(imageViewKilljoy)
+                }
+
+                R.id.radioInitiator -> {
+                    gridLayout.removeAllViews()
+                    gridLayout.addView(imageViewTejo)
+                    gridLayout.addView(imageViewSova)
+                }
+            }
         }
     }
 }
